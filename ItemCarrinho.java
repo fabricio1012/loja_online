97a0e3fff489d6bfadcfa6e1f8f930a957e6ad03
fabricio1012 <128443331+fabricio1012@users.noreply.github.com>
2024-10-03 12:16:28 -0300

    ItemCarrinho.java

--- ItemCarrinho.java
+++ ItemCarrinho.java
@@ -1,1 +1,34 @@
+public class ItemCarrinho {
+    private Produto produto;
+    private int quantidade;
 
+    public ItemCarrinho(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void adicionarQuantidade(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularPrecoTotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return produto.getNome() + ": " + quantidade + " - R$" + String.format("%.2f", calcularPrecoTotal());
+    }
+}
